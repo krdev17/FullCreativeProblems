4e4fd2e84aa4389e51414470dcd5b0b70d36be7e
krdev17 <55844638+krdev17@users.noreply.github.com>
2023-01-19 14:39:23 +0530

    Update Character.java

--- Problem-3/Character.java
+++ Problem-3/Character.java
@@ -1,33 +1,44 @@
 package p1;
+
 public class Character {
     private CharacterType type;
     private int health;
     private int damage;
+
     public CharacterType getType() {
         return type;
     }
+
     public void setType(CharacterType type) {
         this.type = type;
     }
+
     public int getHealth() {
         return health;
     }
+
     public void setHealth(int health) {
         this.health = health;
     }
+
     public int getDamage() {
         return damage;
     }
+
     public void setDamage(int damage) {
         this.damage = damage;
     }
-    Character(CharacterType type, int health){
+
+    Character(CharacterType type, int health) {
         this.type = type;
         this.health = health;
     }
-    public void attacks(Character character, Integer damage) {
+
+    public void attacks(Character character) {
+        int damage = (int) (Math.random() * (50 - 0 + 1)) + 0;
         character.setHealth(character.getHealth() - damage);
     }
+
     public boolean isDead() {
         return this.health <= 0;
     }
